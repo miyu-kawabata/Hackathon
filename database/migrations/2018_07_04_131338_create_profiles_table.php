acbe355515f7932a9ad150e329a9bbc9e9251df1
Ayumi Hanaoka <ayumi.a.hanaoka@rakuten.com>
2018-07-04 05:25:42 +0000

    app

--- database/migrations/2018_07_04_131338_create_profiles_table.php
+++ database/migrations/2018_07_04_131338_create_profiles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateProfilesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('profiles', function (Blueprint $table) {
+            
             $table->increments('id');
-            $table->string('nickname');
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('user_id')->unsigned()->index();
+            $table->string('comment')->nullable();
             $table->timestamps();
+            
+            $table->foreign('user_id')->references('id')->on('users');
+
         });
     }
 
@@ -29,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('profiles');
     }
 }
