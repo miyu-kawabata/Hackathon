ffa4114bdebb7e85f55cc7ccb8be45aaa665fd22
ayaka.kitano <ayaka.kitano@rakuten.com>
2018-07-04 05:25:11 +0000

    groups controller

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('groups', 'GroupsController');
+
+});
