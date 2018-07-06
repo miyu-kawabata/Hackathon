fa0f1742a6dd8d14263d5c4f8355387fb576d74e
ayaka.kitano <ayaka.kitano@rakuten.com>
2018-07-06 04:41:19 +0000

    participants

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,4 +36,10 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+    
+     public function username()
+    {
+        return 'nickname';
+		//'email'を任意のカラム名に変えてください。
+    }
 }
