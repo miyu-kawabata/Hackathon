3ff81bcb08549e6da94e99b3130c5c1d2d738d5c
EC2 Default User <miyu.kawabata@rakuten.com>
2018-07-25 05:23:54 +0000

    fix register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -48,7 +48,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'nickname' => 'required|string|max:191',
+            'nickname' => 'required|string|unique:users|max:191',
             'password' => 'required|string|min:6|confirmed',
         ]);
     }
